18409503f2882a656b8f114f572611bd572187db
小羊 <tlylz99@live.cn>
2021-07-11 22:40:03 +0800

    Add ccs-preview upstream package feed.

--- src/Namomo/Migrations/20210711143120_BunchOfUpdate.Designer.cs
+++ src/Namomo/Migrations/20210711143120_BunchOfUpdate.Designer.cs
@@ -2,16 +2,16 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using SatelliteSite;
 
 namespace SatelliteSite.Migrations
 {
-    [DbContext(typeof(DefaultContext))]
-    partial class DefaultContextModelSnapshot : ModelSnapshot
+    partial class BunchOfUpdate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Namomo/Migrations/20210711143120_BunchOfUpdate.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SatelliteSite.Migrations
+{
+    [DbContext(typeof(DefaultContext))]
+    [Migration("20210711143120_BunchOfUpdate")]
+    public partial class BunchOfUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsEligible",
+                table: "TenantCategory",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Content",
+                table: "ContestEvents",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(2048)",
+                oldMaxLength: 2048);
+
+            migrationBuilder.InsertData(
+                table: "Configurations",
+                columns: new[] { "Name", "Category", "Description", "DisplayPriority", "Public", "Type", "Value" },
+                values: new object[] { "contest_last_rating_change_time", "Contest", "Last rating update time.", 0, false, "datetime", "null" });
+
+            migrationBuilder.UpdateData(
+                table: "TenantCategory",
+                keyColumn: "Id",
+                keyValue: -3,
+                column: "IsEligible",
+                value: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Configurations",
+                keyColumn: "Name",
+                keyValue: "contest_last_rating_change_time");
+
+            migrationBuilder.DropColumn(
+                name: "IsEligible",
+                table: "TenantCategory");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Content",
+                table: "ContestEvents",
+                type: "character varying(2048)",
+                maxLength: 2048,
+                nullable: false,
+                oldClrType: typeof(string));
+        }
+    }
+}
